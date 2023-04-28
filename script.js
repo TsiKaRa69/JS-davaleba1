b7f9c3b28e0be188c3b14072241b822b11ca338b
Lasha Tsikarishvili <109016231+TsiKaRa69@users.noreply.github.com>
2023-04-29 00:22:32 +0300

    br
    br

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-let userAge=100;
-console.log(userAge);
+//1-davaleba
+let number=100;
+if (number<50){
+  console.log('Less than  50');
+} else {
+  console.log('Less than 50');
+} if (number>20){
+  console.log('more than 20');
+} else{
+  console.log('error');
+}
+//2-davaleba
+let result = (nameUser='mariam') ? 'True' : 'error';
+console.log(result);
+if (userName == "mariam") {
+  console.log("True");}
+else {
+   console.log("Error")
+}
+switch(nameUser) {
+  case "mariam"
+  console.log('true');
+ break;
+ default:
+  console.log('error')
+}
+  
